b0abaac1a81d4790f2ec7c6b6224b1594c3345f2
gxj <aini19960402@163.com>
2021-02-27 00:48:54 +0800

    20210227

--- 35. 复杂链表的复制.js
+++ 35. 复杂链表的复制.js
@@ -4,12 +4,13 @@ var copyRandomList = function (head) {
     let node = head
     while (node) {
         m.set(node, new Node(node.val))
+        // * 设置了一对键值对,键名是原始node节点,键值是node节点的val
         node = node.next
     }
     node = head
     while (node) {
         m.get(node).next = node.next ? m.get(node.next) : null
-        // m.get(node)是拿到了Map m中和原链表node.val相同的元素,此时这个元素只有一个val,还没有next和random
+        // * m.get(node),括号里node是键值对的键,get方法是用键拿Map m的键值对的值,     node.next是原链表的next节点,是map中下一个键值对的键,然后外面又m.get,即m.get(node.next)是m.get(下一个键值对的键),拿到下一个键值对的值
         m.get(node).random = node.random ? m.get(node.random) : null
         node = node.next
     }
